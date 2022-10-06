6336e10ecd699c82c07b3cef227bab9931381c13
Innovin <102300150+InnovinAC@users.noreply.github.com>
2022-10-06 23:02:03 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,23 +1,46 @@
 
 $('button').click(function() {
    
+   if($('#screen').val() == 0) {
+      $('#screen').val('');
+     }
+     y = $('#screen').val()
+     
+   
    let x = $(this).attr('id').toLowerCase();
-   let y = $('#screen').val();
+  
+   
+  
+
+
+   
 
    switch(x) {
       
       case '=' : 
+         if(y == '0' || y == '') {
+            $('#screen').val('Nope!')
+
+            setTimeout(function() {
+               $('#screen').val('0')
+            }, 500)
+            break
+         }
+
+         else {
          let value = eval(y).toString();
-         console.log(value);
          $('#screen').val("Ans = " + value)
          break
+         }
 
       case 'del':
+         if(y != '0') {
          $('#screen').val(y.slice(0, -1))
+         }
 
          break
       case 'c':
-         $('#screen').val('')
+         $('#screen').val(0)
          break
       default:
          $('#screen').val(y + x)
@@ -26,6 +49,14 @@ $('button').click(function() {
    }
 });
 
+
+$('input').on('keyup keydown', function() {
+   let allowed = ['0','1','2','3','4','5','6','7','8','9','c','del','=','*','/'];
+   x = $(this).val()
+   if (allowed.indexOf($(this).val().slice(-1)) == -1) {
+      $(this).val(x.slice(0,-1))
+   }
+})
 
 
 
